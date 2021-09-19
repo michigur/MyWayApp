ab34e7778c4c5e9f430cbe1ca02f24c78597e335
User <User@LAB3-19>
2021-09-19 12:03:59 +0300

    Add project files.

---
+++ MyWayApp/MyWayApp/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MyWayServerBL.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserPswd { get; set; }
+    }
+}
